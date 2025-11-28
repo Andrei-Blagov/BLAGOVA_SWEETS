f8a64334cc25a46f9b51807677410709ec68baf2
Andrey Blagov <blagovandrey1323@gmail.com>
2025-11-28 15:47:26 +0300

    added component CookieConsentBanner

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
+  <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtPage />
+    <CookieConsentBanner />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import CookieConsentBanner from '~/components/CookieConsentBanner.vue'
+</script>
+
